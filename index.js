7108de9cfbb415265a11ff56259e1d0f10bdfa64
Shristi Koju <shristi.koju@yipl.com.np>
2026-05-04 14:53:59 +0545

    create 404 page

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ import express from 'express';
 import path from 'path';
 import { home, about, contact } from './pages/home.js';
 import { Form } from './pages/form.js';
+import { fileURLToPath } from 'url';
 const app = express();
 const port = 3000;
+// recreate __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const absPath = path.resolve('html');
 
 
 app.get('/', (req, res) => {
@@ -19,13 +24,17 @@ app.get('/about', (req, res) => {
 });
 
 app.get('/login', (req, res) => {
-    const absPath = path.resolve('html/login.html');
-    res.sendFile(absPath);
+    res.sendFile(absPath + '/login.html');
 });
 
 app.post('/submit', (req, res) => {
     res.send(`Form submitted
         <p><a href="/">Go to home</a></p>`);
+});
+
+// 404 handler
+app.use((req, res) => {
+    res.status(404).sendFile(absPath + '/404.html');
 });
 
 app.listen(port, () => {
